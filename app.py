d0e2698ccf12d5d666c3ee7afc6b5c8bf7009736
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-27 20:10:29 -0500

    feat(block_email): block a given email

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
+
+from .commands.block_email import BlockEmailCommand
 from .errors.errors import ApiError
 from .model.blacklist import NewBlacklistJsonSchema
 loaded = load_dotenv('.env.development')
@@ -19,6 +21,12 @@ def ping():
 def add_email():
     json = request.get_json()
     NewBlacklistJsonSchema.check(json)
+    block_email = BlockEmailCommand(
+        email=json['email'],
+        app_uuid=json['app_uuid'],
+        blocked_reason=json.get('blocked_reason') or None,
+    )
+    block_email.execute()
     return jsonify({ 'message': 'Cuenta creada exitosamente.' }), 200
 
 @app.errorhandler(ApiError)
